74da756e83aa94fc44a6ff2dddbf7a9454986e81
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-03-08 10:26:39 +0900

    [BE_feat] OrderController 내 CRUD 메서드 생성

--- server/src/main/java/com/example/server/order/controller/OrderController.java
+++ server/src/main/java/com/example/server/order/controller/OrderController.java
@@ -1,5 +1,47 @@
 package com.example.server.order.controller;
 
-public class OrderController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/orders")
+@Validated
+public class OrderController {
+  @PostMapping
+  public ResponseEntity postOrder() {
+
+    return new ResponseEntity(HttpStatus.CREATED);
+  }
+
+  @PatchMapping("/{order-id}")
+  public ResponseEntity patchOrder(@PathVariable("order-id") long id) {
+
+    return new ResponseEntity(HttpStatus.OK);
+  }
+
+  @GetMapping("/{order-id}")
+  public ResponseEntity getOrder(@PathVariable("order-id") long id) {
+
+    return new ResponseEntity(HttpStatus.OK);
+  }
+
+  @GetMapping
+  public ResponseEntity getOrders() {
+
+    return new ResponseEntity(HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{order-id}")
+  public ResponseEntity deleteOrder(@PathVariable("order-id") long id) {
+
+    return new ResponseEntity(HttpStatus.NO_CONTENT);
+  }
 }
