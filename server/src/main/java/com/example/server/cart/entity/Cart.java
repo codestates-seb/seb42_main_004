9f0576733f5944abcbab0435232fa663c6fe7eb6
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-03-09 10:20:18 +0900

    [BE_feat] User 충돌 해결

--- server/src/main/java/com/example/server/cart/entity/Cart.java
+++ server/src/main/java/com/example/server/cart/entity/Cart.java
@@ -1,9 +1,16 @@
 package com.example.server.cart.entity;
 
 import com.example.server.baseEntity.BaseEntity;
+import com.example.server.user.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 public class Cart extends BaseEntity {
   @Id
@@ -12,4 +19,10 @@ public class Cart extends BaseEntity {
 
   private int totalPrice;
 
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @OneToMany(mappedBy = "cart")
+  private List<CartMealbox> cartMealboxes = new ArrayList<>();
 }
