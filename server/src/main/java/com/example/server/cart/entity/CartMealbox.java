9a5b2491ed814261f077bc59fd8ea71a7b1e6b23
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-03-09 18:12:59 +0900

    [BE_fix] 오타수정

--- server/src/main/java/com/example/server/cart/entity/CartMealbox.java
+++ server/src/main/java/com/example/server/cart/entity/CartMealbox.java
@@ -1,12 +1,12 @@
 package com.example.server.cart.entity;
 
 import com.example.server.mealbox.entity.Mealbox;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,11 +22,11 @@ public class CartMealbox {
   private long cartMealboxId;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @Column(name = "cart_id")
+  @JoinColumn(name = "cart_id")
   private Cart cart;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @Column(name = "mealbox_id")
+  @JoinColumn(name = "mealbox_id")
   private Mealbox mealbox;
 
   private int quantity;
