ad11b8c795b6dd2766e76eddc1a7868bdf3fd8e2
myungju <myungju030@gmail.com>
2023-03-10 11:30:36 +0900

    [FE_fix] 충돌 해결

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,6 @@ import Header from './components/commons/Header';
 import GlobalStyle from './global/globalstyles';
 import Footer from './components/commons/Footer';
 import AllBoxes from './pages/AllBoxes';
-import RecommendedBox from './pages/RecommendedBox';
-import Custom from './pages/Custom';
 import Cart from './pages/Cart';
 
 function App() {
@@ -15,8 +13,6 @@ function App() {
       <div className="marginbase bodymargin">
         <Routes>
           <Route path="/" element={<AllBoxes />} />
-          <Route path="/survey/recommend" element={<RecommendedBox />} />
-          <Route path="/custom" element={<Custom />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </div>
