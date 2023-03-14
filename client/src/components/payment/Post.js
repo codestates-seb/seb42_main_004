e91d7578805ec4bb0c61c5bfeacfe7ca07bfc8fa
myungju <myungju030@gmail.com>
2023-03-14 20:44:42 +0900

    [FE_feat] 주소검색 기능 구현

---
+++ client/src/components/payment/Post.js
@@ -0,0 +1,42 @@
+import DaumPostcode from 'react-daum-postcode';
+import styled from 'styled-components';
+
+const Post = (props) => {
+  const complete = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = '';
+
+    if (data.addressType === 'R') {
+      if (data.bname !== '') {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== '') {
+        extraAddress +=
+          extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
+    }
+    console.log(data);
+    console.log(fullAddress);
+    console.log(data.zonecode);
+
+    props.setcompany({
+      ...props.company,
+      address: fullAddress,
+      zonecode: data.zonecode,
+    });
+  };
+
+  return (
+    <div>
+      <Postcode autoClose onComplete={complete} />
+    </div>
+  );
+};
+
+export default Post;
+
+const Postcode = styled(DaumPostcode)`
+  background: rgba(0, 0, 0, 0.25);
+  position: fixed;
+`;
