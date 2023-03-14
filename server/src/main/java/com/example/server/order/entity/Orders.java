691f296b27c436e2f082ee0767d5296e516a537c
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-03-15 00:56:17 +0900

    [BE_feat] PayInfo 연관관계 매핑, 상태 변경 메서드 추가

--- server/src/main/java/com/example/server/order/entity/Orders.java
+++ server/src/main/java/com/example/server/order/entity/Orders.java
@@ -2,8 +2,11 @@ package com.example.server.order.entity;
 
 import com.example.server.baseEntity.BaseEntity;
 import com.example.server.order.data.OrderStatus;
+import com.example.server.payment.Entity.PayInfo;
 import com.example.server.user.entity.User;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
@@ -16,9 +19,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
 
 @NoArgsConstructor
 @Getter
@@ -43,7 +48,7 @@ public class Orders extends BaseEntity {
 
   private String phoneNumber; // 수령인 전화번호
 
-  private OrderStatus status = OrderStatus.ORDER_COMPLETED; // 주문 상태
+  private OrderStatus status = OrderStatus.NOT_PAID; // 주문 상태
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "USER_ID")
@@ -66,14 +71,39 @@ public class Orders extends BaseEntity {
     }
   }
 
-  public void applyRefund() {
-    this.status = OrderStatus.REFUND_APPLIED;
+  @OneToOne(mappedBy = "order")
+  private PayInfo payInfo;
+
+  public void addPayInfo(PayInfo payInfo) {
+    this.payInfo = payInfo;
+    if(payInfo.getOrder() != this) {
+      payInfo.addOrder(this);
+    }
   }
+
+  public void paid() {
+    this.status = OrderStatus.ORDER_COMPLETED;
+  }
+
+  public void cancelOrder() {
+    this.status = OrderStatus.ORDER_CANCELED;
+  }
+
   public void completeDelivery() {
     this.status = OrderStatus.DELIVERY_COMPLETED;
     deliveryDate = LocalDate.now();
   }
-  public void cancelOrder() {
-    this.status = OrderStatus.ORDER_CANCELED;
+
+  public void applyRefund() {
+    this.status = OrderStatus.REFUND_APPLIED;
+  }
+
+  public void refundOrder() {
+    this.status = OrderStatus.REFUNDED;
+  }
+
+  public void makeOrderNumber() {
+    String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+    this.orderNumber = date + RandomStringUtils.randomNumeric(6);
   }
 }
