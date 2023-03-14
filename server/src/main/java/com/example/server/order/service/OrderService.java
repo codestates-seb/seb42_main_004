d529b35f173f09d263672435902219e20c673f93
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-03-14 09:15:06 +0900

    [BE_refactor] OrderException 추가

--- server/src/main/java/com/example/server/order/service/OrderService.java
+++ server/src/main/java/com/example/server/order/service/OrderService.java
@@ -3,6 +3,7 @@ package com.example.server.order.service;
 import com.example.server.exception.BusinessLogicException;
 import com.example.server.exception.ExceptionCode;
 import com.example.server.order.entity.Orders;
+import com.example.server.order.exception.OrderException;
 import com.example.server.order.repository.OrderMealboxRepository;
 import com.example.server.order.repository.OrderRepository;
 import java.time.LocalDate;
@@ -29,7 +30,7 @@ public class OrderService {
     if(index == 3) {
       order.applyRefund();
     } else if (index == 4 & LocalDate.now().isAfter(order.getDeliveryDate().plusDays(1))) {
-      throw new BusinessLogicException(ExceptionCode.NOT_REFUNDABLE_DATE);
+      throw new BusinessLogicException(OrderException.NOT_REFUNDABLE_DATE);
     } else if (index == 4) {
       order.applyRefund();
     } else if (index == 1) {
@@ -43,7 +44,7 @@ public class OrderService {
     Orders order = findVerifiedOrder(orderId);
     int index = order.getStatus().getIndex();
     if(index != 3) {
-      throw new BusinessLogicException(ExceptionCode.DELIVERY_IS_NOT_IN_PROGRESS);
+      throw new BusinessLogicException(OrderException.DELIVERY_IS_NOT_IN_PROGRESS);
     }
     order.completeDelivery();
     return orderRepository.save(order);
@@ -55,7 +56,7 @@ public class OrderService {
 
   private Orders findVerifiedOrder(long orderId) {
     Optional<Orders> order = orderRepository.findById(orderId);
-    Orders findOrder = order.orElseThrow(() -> new BusinessLogicException(ExceptionCode.ORDER_NOT_FOUND));
+    Orders findOrder = order.orElseThrow(() -> new BusinessLogicException(OrderException.ORDER_NOT_FOUND));
     return findOrder;
   }
 }
