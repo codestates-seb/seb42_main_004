002368029a7f8e95839f888e69009c1eac15e76b
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-03-15 15:53:46 +0900

    [BE_feat] OrderRepository GET 요청에 쓸 메서드 완성

--- server/src/main/java/com/example/server/order/repository/OrderRepository.java
+++ server/src/main/java/com/example/server/order/repository/OrderRepository.java
@@ -1,16 +1,19 @@
 package com.example.server.order.repository;
 
 import com.example.server.order.entity.Orders;
+import com.example.server.user.entity.User;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
   //나중에 테스트 해봐야함
-//  Page<Orders> findAllByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
-//  List<Orders> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
+  Page<Orders> findAllByCreatedDateBetween(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
+//  @Query(value = "SELECT o FROM Orders o WHERE o.createdDate between :startDate and :endDate")
+  List<Orders> findByCreatedDateBetweenAndUserOrderByCreatedDateDesc(LocalDateTime startDate, LocalDateTime endDate, User user);
   Optional<Orders> findByOrderNumber(String orderNumber);
 }
