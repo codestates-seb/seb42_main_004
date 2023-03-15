e317734c043f325e1a73992223f7ccec943b2d82
myungju <myungju030@gmail.com>
2023-03-15 22:56:46 +0900

    [FE_feat] util 함수 생성(get,post,patch,delete)

---
+++ client/src/util/patchData.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+async function patchData(url, data) {
+  try {
+    const response = await axios.patch(
+      `${process.env.REACT_APP_API_URL}${url}`,
+      JSON.stringify(data),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default patchData;
---
+++ client/src/util/postData.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+async function postData(url, data) {
+  try {
+    const response = await axios.post(
+      `${process.env.REACT_APP_API_URL}${url}`,
+      JSON.stringify(data),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default postData;
