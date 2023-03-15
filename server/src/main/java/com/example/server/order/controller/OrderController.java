5e5e2d50c962413c9ca6e8f4ca14f96cbd02df57
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-03-15 19:52:09 +0900

    [BE_feat] OrderController GET 요청 메서드 추가

--- server/src/main/java/com/example/server/order/controller/OrderController.java
+++ server/src/main/java/com/example/server/order/controller/OrderController.java
@@ -1,13 +1,19 @@
 package com.example.server.order.controller;
 
+import com.example.server.dto.MultiResponseDto;
+import com.example.server.order.dto.OrderGetDto;
 import com.example.server.order.dto.OrderPostDto;
+import com.example.server.order.dto.OrderResponseDto;
 import com.example.server.order.entity.Orders;
 import com.example.server.order.mapper.OrderMapper;
 import com.example.server.order.service.OrderService;
 import com.siot.IamportRestClient.exception.IamportResponseException;
 import java.io.IOException;
+import java.util.List;
+import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,7 +23,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -41,15 +47,17 @@ public class OrderController {
   }
 
   @GetMapping("/orders/user/{user-id}")
-  public ResponseEntity getOrder(@PathVariable("user-id") @PositiveOrZero long userId) {  //유저별 주문 내역 확인, JWT 정보랑 비교해야할 듯
-
-    return new ResponseEntity(HttpStatus.OK);
+  public ResponseEntity getOrdersUser(@PathVariable("user-id") @PositiveOrZero long userId, @RequestBody OrderGetDto orderGetDto) {  //유저별 주문 내역 확인, JWT 정보랑 비교해야할 듯
+    List<Orders> orders = orderService.getOrdersByDateToList(orderGetDto, userId);
+    return new ResponseEntity(orders, HttpStatus.OK);
   }
 
   @GetMapping("/admin/orders")
-  public ResponseEntity getOrders() {
-
-    return new ResponseEntity(HttpStatus.OK);
+  public ResponseEntity getOrdersAdmin(@RequestParam @Positive int page, @RequestBody OrderGetDto orderGetDto) {
+    Page<Orders> orders = orderService.getOrdersByDateToPage(orderGetDto, page-1);
+    List<Orders> content = orders.getContent();
+    List<OrderResponseDto> orderResponseDtos = mapper.OrdersToOrderResponseDtos(content);
+    return new ResponseEntity<>(new MultiResponseDto<>(orderResponseDtos, orders),HttpStatus.OK);
   }
 
   @DeleteMapping("/orders/{order-id}")
