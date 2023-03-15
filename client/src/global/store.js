63d3e3239fcf014658ff195820fbd72f12b34341
YUNH7 <yunny126@gmail.com>
2023-03-15 22:00:33 +0900

    [FE_feat] cart, custom, surveyRcmd 전역 상태 추가

--- client/src/global/store.js
+++ client/src/global/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import exampleReducer from '../reducers/exampleReducer';
+import cartReducer from '../reducers/cartReducer';
+import customReducer from '../reducers/customReducer';
+import surveyRcmdReducer from '../reducers/surveyRcmdReducer';
 
 const store = configureStore({
   reducer: {
     exampleReducer,
+    cartReducer,
+    customReducer,
+    surveyRcmdReducer,
   },
 });
 
