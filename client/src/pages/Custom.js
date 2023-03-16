b42466cb1cccdda8557a49aca59dc5e782017c8b
YUNH7 <yunny126@gmail.com>
2023-03-16 14:51:34 +0900

    [FE_refactor] CartAside 구조 변경

--- client/src/pages/Custom.js
+++ client/src/pages/Custom.js
@@ -43,27 +43,33 @@ function Custom({ admin, mealBox }) {
           }
           buttonClick={admin ? () => setOpenModal(true) : () => navigate('/')}
         >
-          <InAsideH2>Custom</InAsideH2>
-          <InAsideUl>
-            <ElementInBucketLi>
-              <span>{`${'오렌지주스'}`}</span>
-              <span>
-                {`${1}`}
-                <TextButton className="linkstyle">&#10005;</TextButton>
-              </span>
-            </ElementInBucketLi>
-            <ElementInBucketLi>
-              <span>{`${'오렌지주스'}`}</span>
-              <span>
-                {`${1}`}
-                <TextButton className="linkstyle">&#10005;</TextButton>
-              </span>
-            </ElementInBucketLi>
-          </InAsideUl>
-          <InAsideDiv>
-            <span>합계</span>
-            <span>{`${'19,900'}원`}</span>
-          </InAsideDiv>
+          <InAsideBoxDiv>
+            <TriangleDiv />
+            <InAsideTitleDiv className="hidden">
+              <h2>Custom</h2>
+              <span>99,999kcal</span>
+            </InAsideTitleDiv>
+            <ul>
+              <ElementInBucketLi>
+                <span>{`${'오렌지주스'}`}</span>
+                <span>
+                  {`${1}`}
+                  <TextButton className="linkstyle">&#10005;</TextButton>
+                </span>
+              </ElementInBucketLi>
+              <ElementInBucketLi>
+                <span>{`${'오렌지주스'}`}</span>
+                <span>
+                  {`${1}`}
+                  <TextButton className="linkstyle">&#10005;</TextButton>
+                </span>
+              </ElementInBucketLi>
+            </ul>
+            <InAsidePriceDiv>
+              <span>합계</span>
+              <span>{`${'19,900'}원`}</span>
+            </InAsidePriceDiv>
+          </InAsideBoxDiv>
         </CartAside>
       </CustomSelectDiv>
     </MealBoxesWrapDiv>
@@ -90,29 +96,73 @@ const BoxElementCardUl = styled.ul`
   list-style: none;
   margin-bottom: -10px;
 `;
-const InAsideH2 = styled.h2`
-  color: var(--white);
-  list-style: none;
-  min-height: 1vh;
+const InAsideBoxDiv = styled.div`
+  background-color: var(--bucket_brown);
+  padding: 1rem;
+  border-radius: 10px 10px 0 0;
+  box-shadow: 0 0 0 2px var(--bucket_brown) inset,
+    2px 2px 2px rgba(0, 0, 0, 0.4);
 
   @media (max-width: 480px) {
-    display: none;
+    border-radius: 0;
+
+    .hidden,
+    > ul {
+      display: none;
+    }
+
+    > div {
+      margin: 0;
+    }
+
+    :hover {
+      border-radius: 10px 10px 0 0;
+
+      div:first-child {
+        display: none;
+      }
+
+      .hidden {
+        display: flex;
+      }
+
+      > ul {
+        display: block;
+      }
+    }
   }
 `;
+const TriangleDiv = styled.div`
+  display: none;
+  width: 0;
+  height: 0;
+  border-bottom: calc(4px * 1.732) solid var(--bucket_brown);
+  border-left: 8px solid transparent;
+  border-right: 8px solid transparent;
+  position: absolute;
+  top: -6.4px;
+  left: 50%;
 
-const InAsideUl = styled.ul`
   @media (max-width: 480px) {
-    display: none;
+    display: block;
   }
 `;
-
-export const InAsideDiv = styled.div`
+const InAsideTitleDiv = styled.div`
   display: flex;
-  justify-content: space-between;
-  margin-top: 1rem;
-  margin-bottom: -0.5rem;
-`;
+  align-items: baseline;
 
+  > span {
+    font-size: 0.8rem;
+    margin-left: 0.5rem;
+    font-weight: normal;
+  }
+
+  > h2 {
+    color: var(--white);
+    list-style: none;
+    min-height: 1vh;
+  }
+`;
 const ElementInBucketLi = styled.li`
   display: flex;
   justify-content: space-between;
@@ -126,8 +176,10 @@ const ElementInBucketLi = styled.li`
       margin-left: 0.5rem;
     }
   }
-
-  @media (max-width: 480px) {
-    border-radius: 0;
-  }
+`;
+const InAsidePriceDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1rem;
+  margin-bottom: -0.5rem;
 `;
