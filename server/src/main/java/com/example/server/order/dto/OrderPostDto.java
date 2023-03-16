e84f43d97cf35ffe69fd743f4ec870953832d2e9
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-03-16 15:55:27 +0900

    [BE_feat] OrderPostDto 내 변수 생성

--- server/src/main/java/com/example/server/order/dto/OrderPostDto.java
+++ server/src/main/java/com/example/server/order/dto/OrderPostDto.java
@@ -1,7 +1,12 @@
 package com.example.server.order.dto;
 
-public class OrderPostDto {
-  // 총액
-  // 밀박스 정보 + 재료 정보
-  // 유저 ID
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class OrderPostDto { // 장바구니에서 주문으로 넘어갈 때 필요 (Order 엔티티 생성)
+  private int totalPrice;
+  private long userId;
+  private List<OrderMealboxPostDto> mealboxes;
+
 }
