ad7155f2965c04842e4438daf49ed94f03f3841a
WiJunSeong <baram2449@gmail.com>
2023-03-17 09:27:18 +0900

    [BE_feat] initDB에 passwordEncoder 적용

--- server/src/main/java/com/example/server/init/InitDB.java
+++ server/src/main/java/com/example/server/init/InitDB.java
@@ -10,6 +10,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -31,17 +32,21 @@ class InitUser {
 
   private final CustomAuthorityUtils authorityUtils;
   private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
   public void UserInit() {
     List<User> user = new ArrayList<>();
 
-    user.add(User.builder().id(1L).email("admin@gmail.com").password("qwe123").name("관리자").roles(
+    user.add(User.builder().id(1L).email("admin@gmail.com").password(
+            passwordEncoder.encode("admin")).name("관리자").roles(
             authorityUtils.createRoles("admin@gmail.com")).address(new Address()).status(UserStatus.USER_ADMIN)
         .phoneNumber("010-1234-1234").build());
-    user.add(User.builder().id(2L).email("qwe@gmail.com").password("qwe123").name("위준성").roles(
+    user.add(User.builder().id(2L).email("qwe@gmail.com").password(
+            passwordEncoder.encode("qwe123")).name("위준성").roles(
             authorityUtils.createRoles("qwe@gmail.com")).address(new Address()).mailKey("QWERQWERQW")
         .phoneNumber("010-1234-1234").build());
-    user.add(User.builder().id(3L).email("asd@gmail.com").password("asd123").name("진태양").roles(
+    user.add(User.builder().id(3L).email("asd@gmail.com").password(
+            passwordEncoder.encode("asd123")).name("진태양").roles(
             authorityUtils.createRoles("asd@gmail.com")).address(new Address())
         .phoneNumber("010-1234-1234").build());
 
