7a6eacb87107726b10e3d65275386a8c81e372d2
qwerty00ui88 <qwerty0.0ui88@gmail.com>
2023-03-19 02:28:15 +0900

    [FE_feat] session storage 저장 기능 구현

--- client/src/global/store.js
+++ client/src/global/store.js
@@ -1,16 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import exampleReducer from '../reducers/exampleReducer';
 import customReducer from '../reducers/customReducer';
 import surveyRcmdReducer from '../reducers/surveyRcmdReducer';
 import surveyQuestionReducer from '../reducers/surveyQuestionReducer';
+import { persistReducer } from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session';
+import thunk from 'redux-thunk';
 
-const store = configureStore({
-  reducer: {
-    exampleReducer,
-    customReducer,
-    surveyRcmdReducer,
-    surveyQuestionReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage: storageSession,
+  whitelist: ['surveyQuestionReducer'],
+};
+
+export const rootReducer = combineReducers({
+  exampleReducer,
+  customReducer,
+  surveyRcmdReducer,
+  surveyQuestionReducer,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
+});
