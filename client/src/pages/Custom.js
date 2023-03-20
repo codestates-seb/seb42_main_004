91f9be4cf6764fceac484739b0a2de5d7414765f
YUNH7 <yunny126@gmail.com>
2023-03-20 15:52:47 +0900

    [FE_feat] Custom uri 연결

--- client/src/pages/Custom.js
+++ client/src/pages/Custom.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import ModalDiv from '../components/commons/ModalDiv';
@@ -7,44 +7,84 @@ import PaginationUl from '../components/commons/PaginationUl';
 import FilterSearchDiv from '../components/commons/FilterSearchDiv';
 import BoxElementCardDiv from '../components/custom/BoxElementCardDiv';
 import { MealBoxesWrapDiv } from './AllBoxes';
-// import useGET from '../util/useGET';
+import useGET from '../util/useGET';
+import GetTemplate from '../components/commons/GetTemplate';
 
 function Custom({ admin }) {
   const [openModal, setOpenModal] = useState(false);
   const [page, setPage] = useState(1);
-  // const [products, isPending, error] = useGET(`/products/${page}`);
+  const [sortBy, setSortBy] = useState(['id', 'ASC']);
+  const [searchWord, setSearchWord] = useState('');
+  const [path, setPath] = useState('page=1&sort=id&dir=ASC');
+  const [res, isPending, error] = useGET(`/products?${path}`);
   const navigate = useNavigate();
 
+  const searchProducts = () => {
+    setPage(1);
+    getProducts();
+  };
+
+  const sortProducts = (select) => {
+    setSearchWord('');
+    setSortBy(select);
+    setPage(1);
+    getProducts();
+  };
+
+  const getProducts = () => {
+    if (!searchWord) setPath(`page=${page}&sort=${sortBy[0]}&dir=${sortBy[1]}`);
+    else setPath(`page=${page}&search=${searchWord}`);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [page]);
+
   return (
-    <CustomWrapDiv className="margininside">
-      <ModalDiv
+    <GetTemplate isPending={isPending} error={error} res={res.data}>
+      <CustomWrapDiv className="margininside">
+        {/* <ModalDiv
         mealBox={0}
         boxElement={1}
         closeModal={() => setOpenModal(false)}
-      />
-      {admin && openModal && (
-        <ModalDiv closeModal={() => setOpenModal(false)} />
-      )}
-      <h1>커스텀 밀박스</h1>
-      <CustomSelectDiv>
-        <ElementsContainerDiv>
-          <FilterSearchDiv />
-          <BoxElementCardUl>
-            <li>
-              <BoxElementCardDiv />
-            </li>
-          </BoxElementCardUl>
-          <PaginationUl page={page} totalpage={3} setPage={setPage} />
-        </ElementsContainerDiv>
-        <CustomAside
-          admin={0}
-          bucket={1}
-          buttonClick={
-            admin ? () => setOpenModal(true) : () => navigate('/cart')
-          }
-        />
-      </CustomSelectDiv>
-    </CustomWrapDiv>
+      /> */}
+        {admin && openModal && (
+          <ModalDiv closeModal={() => setOpenModal(false)} />
+        )}
+        <h1>커스텀 밀박스</h1>
+        <CustomSelectDiv>
+          <ElementsContainerDiv>
+            <FilterSearchDiv
+              sortProducts={sortProducts}
+              searchProducts={searchProducts}
+              setSearchWord={setSearchWord}
+            />
+            <BoxElementCardUl>
+              {res?.data?.map((product) => (
+                <li key={product.productId}>
+                  <BoxElementCardDiv />
+                </li>
+              ))}
+              <li>
+                <BoxElementCardDiv />
+              </li>
+            </BoxElementCardUl>
+            <PaginationUl
+              page={res?.pageInfo?.page}
+              totalpage={res?.pageInfo?.totalPages}
+              setPage={setPage}
+            />
+          </ElementsContainerDiv>
+          <CustomAside
+            admin={0}
+            bucket={1}
+            buttonClick={
+              admin ? () => setOpenModal(true) : () => navigate('/cart')
+            }
+          />
+        </CustomSelectDiv>
+      </CustomWrapDiv>
+    </GetTemplate>
   );
 }
 
