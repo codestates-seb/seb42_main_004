0b34126b7466143df92c58a42f7b5292f01b9a47
YUNH7 <yunny126@gmail.com>
2023-03-21 11:41:42 +0900

    [FE_feat] postData, patchData 인자 추가 (multipart 헤더)

--- client/src/util/postData.js
+++ client/src/util/postData.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-async function postData(url, data) {
+async function postData(url, data, multipart) {
   try {
     const response = await axios.post(
       `${process.env.REACT_APP_API_URL}${url}`,
       JSON.stringify(data),
       {
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': `${
+            multipart ? 'multipart/form-data' : 'application/json'
+          }`,
         },
         withCredentials: true,
       }
