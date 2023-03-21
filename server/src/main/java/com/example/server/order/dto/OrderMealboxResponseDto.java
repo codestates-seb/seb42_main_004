ca48f4667f3e26f84964ea8e7980ef574a898fe4
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-03-21 22:08:25 +0900

    [BE_fix] 에러픽스

--- server/src/main/java/com/example/server/order/dto/OrderMealboxResponseDto.java
+++ server/src/main/java/com/example/server/order/dto/OrderMealboxResponseDto.java
@@ -2,9 +2,13 @@ package com.example.server.order.dto;
 
 import com.example.server.mealbox.dto.MealboxProductResponseDto;
 import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
+@Getter
+@NoArgsConstructor
 public class OrderMealboxResponseDto {  // 주문 목록 조회시 필요
     private String mealboxName;
     private int mealboxPrice;
