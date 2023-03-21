7c51e926e3a70594537bb8a70eb7ab88f912e6c3
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-03-22 01:36:22 +0900

    [BE_feat] 주문 페이지 ResponseDto에 수신자 연락처 추가

--- server/src/main/java/com/example/server/order/dto/OrderPageResponseDto.java
+++ server/src/main/java/com/example/server/order/dto/OrderPageResponseDto.java
@@ -16,6 +16,7 @@ public class OrderPageResponseDto { // 결제창에서 넘겨줄 값
   private String deliveryZipCode; // 수신자 우편번호
   private String deliverySimpleAddress; // 수신자 주소
   private String deliveryDetailAddress; // 수신자 상세주소
+  private String deliveryPhoneNumber; // 수신자 연락처
   private int totalPrice; // 총 결제금액
   private String email; // 주문자 이메일
 }
