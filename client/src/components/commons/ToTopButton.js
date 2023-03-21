870f6b6691dcb19b5a174f3458a9235019a1ce17
YUNH7 <yunny126@gmail.com>
2023-03-21 14:00:55 +0900

    [FE_feat] ToTopButton 스크롤이 있을 때만 보이게

--- client/src/components/commons/ToTopButton.js
+++ client/src/components/commons/ToTopButton.js
@@ -1,13 +1,38 @@
+import { useEffect, useState } from 'react';
 import { VscTriangleUp } from 'react-icons/vsc';
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import checkFooter from '../../util/checkFooter';
+
 function Component() {
+  const { path } = useLocation();
+  const [scroll, setScroll] = useState();
+
   const toTop = () => {
     window.scrollTo(0, 0);
   };
+  // resize, page이동
+  const calcHeight = () => {
+    setScroll(document.body.scrollHeight > window.innerHeight);
+  };
+
+  useEffect(() => {
+    calcHeight();
+  }, [path]);
+
+  useEffect(() => {
+    window.addEventListener('resize', calcHeight);
+    return () => {
+      window.removeEventListener('resize', calcHeight);
+    };
+  }, []);
 
   return (
-    <Button onClick={toTop} height={checkFooter() ? 1 : null}>
+    <Button
+      onClick={toTop}
+      scroll={scroll}
+      havefooter={checkFooter() ? 1 : null}
+    >
       <VscTriangleUp />
     </Button>
   );
@@ -16,6 +41,7 @@ function Component() {
 export default Component;
 
 const Button = styled.button`
+  display: ${(props) => (props.scroll ? 'block' : 'none')};
   z-index: 15;
   width: 35px;
   height: 35px;
@@ -30,6 +56,6 @@ const Button = styled.button`
   }
 
   @media screen and (max-width: 480px) {
-    bottom: calc(${(props) => (props.height ? '76px' : '0px')} + 1rem);
+    bottom: calc(${(props) => (props.havefooter ? '76px' : '0px')} + 1rem);
   }
 `;
