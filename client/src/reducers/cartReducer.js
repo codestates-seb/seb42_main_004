c07554c3f4bb1dbc8c8a0b849d3aeba8e918e5fd
YUNH7 <yunny126@gmail.com>
2023-03-22 18:12:55 +0900

    [FE_fix] cartReducer 비로그인장바구니용 리듀서 생성

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -1,16 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = { data: {} };
-//{totalPrice: 25000, mealboxes:{}, {}... }}
+//{totalPrice: 25000, mealboxes:[{}, {}... {}]}
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+    addCart: (state, action) => {
+      state.cart.push(action.payload);
+    },
+    deleteCart: (state, action) => {
+      state.cart = state.cart.filter((box) => box.id !== action.payload.id);
+    },
     setCart: (state, action) => {
-      state.data = action.payload;
+      const { id, num } = action.payload;
+      if (num === 0) return;
+      let { cart } = state;
+      const idx = cart.products.map((product) => product.id).indexOf(id);
+      cart[idx].quantity = num;
     },
   },
 });
 
-export const { setCart } = cartSlice.actions;
+export const { addCart, deleteCart, setCart } = cartSlice.actions;
+
 export default cartSlice.reducer;
