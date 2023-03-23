9a7295c1a72b7431cf951a83aa14e48ed7c1d1cf
myungju <myungju030@gmail.com>
2023-03-23 17:16:37 +0900

    [FE_feat] authReducer, parseToken, setAuthorizationToken 생성

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    isLogin: false,
+    accessToken: '',
+    tokenExpirationDate: '',
+    user: {},
+    roles: [],
+  },
+  reducers: {
+    setAuth: (state, action) => {
+      state.isLogin = action.payload.isLogin;
+      state.accessToken = action.payload.accessToken;
+      state.tokenExpirationDate = action.payload.tokenExpirationDate;
+      state.user = action.payload.user;
+      state.roles = action.payload.roles;
+    },
+    setIsLogin: (state, action) => {
+      state.isLogin = action.payload;
+    },
+    setAccessToken: (state, action) => {
+      state.accessToken = action.payload;
+    },
+    setTokenExpirationDate: (state, action) => {
+      state.tokenExpirationDate = action.payload;
+    },
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    setRoles: (state, action) => {
+      state.roles = action.payload;
+    },
+  },
+});
+
+export const {
+  setAuth,
+  setIsLogin,
+  setAccessToken,
+  setTokenExpirationDate,
+  setUser,
+  setRoles,
+} = authSlice.actions;
+export default authSlice.reducer;
