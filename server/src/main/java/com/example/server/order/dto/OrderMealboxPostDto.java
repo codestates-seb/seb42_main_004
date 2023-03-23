bb1076c5322322254f50f20bc41265aa16db426e
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-03-23 21:27:12 +0900

    [BE_comment] 주석 오타 수정

--- server/src/main/java/com/example/server/order/dto/OrderMealboxPostDto.java
+++ server/src/main/java/com/example/server/order/dto/OrderMealboxPostDto.java
@@ -3,7 +3,7 @@ package com.example.server.order.dto;
 import lombok.Getter;
 
 @Getter
-public class OrderMealboxPostDto {  // OrderPostDto에 들어감가
+public class OrderMealboxPostDto {  // OrderPostDto에 들어감
   private long cartMealboxId;
   private long mealboxId;
   private int quantity;
