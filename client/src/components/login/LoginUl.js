e3178aa1645016102086e661d1fbdd41e2c448c0
myungju <myungju030@gmail.com>
2023-03-27 10:30:30 +0900

    [FE_fix] 로그인 기능 수정

--- client/src/components/login/LoginUl.js
+++ client/src/components/login/LoginUl.js
@@ -96,7 +96,9 @@ function LoginUl() {
           setInputValue({ ...inputValue, password: '' });
           inputRef.current[1].focus();
         } else {
-          login(res.headers.authorization);
+          if (res.headers.authorization) {
+            login(res.headers.authorization);
+          }
         }
       });
     } else if (!email) {
